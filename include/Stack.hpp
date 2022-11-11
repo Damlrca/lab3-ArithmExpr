e92eac237cc612a7957191958ef5b2edc1492f8e
Damlrca <sir.damirca@yandex.ru>
2022-11-11 22:31:10 +0300

    update Stack, add Token

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,10 +1,37 @@
+#ifndef __STACK_HPP__
+#define __STACK_HPP__
+
+template<class T>
 class Stack {
-	int p[10];
+private:
+	T *p;
 	int top;
+	int size;
 public:
-	Stack() : top(-1) {}
+	Stack(int _size) : size{ _size }, top{-1} {
+		if (size <= 0)
+			throw -1;
+		p = new T[size];
+	}
+
 	bool is_empty() { return top == -1; }
-	bool is_full() { return top >= 9; }
-	int pop() { return p[top--]; }
-	void push(int x) { p[++top] = x; }
+	bool is_full() { return top + 1 >= size; }
+
+	int pop() {
+		if (is_empty())
+			throw -1;
+		return p[top--];
+	}
+
+	void push(int x) {
+		if (is_full())
+			throw -1;
+		p[++top] = x;
+	}
+
+	~Stack() {
+		delete[] p;
+	}
 };
+
+#endif // !__STACK_HPP__
--- include/parser.hpp
+++ include/parser.hpp
@@ -1,9 +1,60 @@
+#ifndef __PARSER_HPP__
+#define __PARSER_HPP__
+
 #include "Stack.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+enum class TokenType {
+	Operator,
+	Value,
+	Name
+};
+
+string TokenType_name(TokenType x) {
+	switch (x)
+	{
+	case TokenType::Operator:
+		return "operator";
+		break;
+	case TokenType::Value:
+		return "value";
+		break;
+	case TokenType::Name:
+		return "name";
+		break;
+	default:
+		return "unknown";
+		break;
+	}
+}
+
+class Token {
+	TokenType type;
+	string str;
+public:
+	Token(TokenType t, const string& s) : type{ t }, str{ s } {}
+	string get_str() { return str; }
+	TokenType get_type() { return type; }
+	friend ostream& operator<<(ostream& out, const Token& t) {
+		out << "{" << t.str << ", " << TokenType_name(t.type) << "}";
+		return out;
+	}
+};
+
+vector<Token> lex(string input) {
+	vector<Token> res;
+
+	return res;
+}
 
 template<class T>
 class A {
 	T a;
-	Stack s;
+	Stack<int> s{ 10 };
 public:
 	A(int _a) : a(_a) {
 		for (int i = 0; i < 10; i++)
@@ -12,3 +63,5 @@ public:
 	T get() { return a; }
 	int pop() { return s.pop(); }
 };
+
+#endif __PARSER_HPP__
