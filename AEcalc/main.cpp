eceb1b87c35fd4aec245f69f546871d4e83b3bbe
Damlrca <sir.damirca@yandex.ru>
2022-11-25 23:35:31 +0300

    '=' operator in ArithmExpr::calculate

--- AEcalc/main.cpp
+++ AEcalc/main.cpp
@@ -10,31 +10,40 @@ using namespace std;
 int main() {
 	string s[]{ {"(12e-1 + 13.25e1) -.23e2 * (3 + 2)"}
 				,{"(10*(-25)+13.53 - (-0.12))*32 + 32/0.5"}
+				,{"a"}
+				,{"a = 2"}
+				,{"b = 3"}
 				,{"a = b"}
+				,{"c = 1"}
 				,{"a = b = c"}
+				,{"c = 4"}
 				,{"(a=b)=c"}
+				,{"a"}
+				,{"b"}
+				,{"c"}
 	};
 	//string s{ ";-24" };
 	for (string& c : s) {
 		cout << "input: \"" << c << "\"" << endl;
 		//auto t = get_ArithmExpr_vector(c);
-		cout << "infix:" << endl;
 		auto t1 = lex(c);
+		auto t2 = parse(t1);
+		/*cout << "infix:" << endl;
 		for (auto o : t1)
 			cout << o << endl;
 		cout << "postfix:" << endl;
-		auto t2 = parse(t1);
 		for (auto o : t2)
-			cout << o << endl;
-		/*cout << "ArithmExpr:\n" << endl;
+			cout << o << endl;*/
+		//cout << "ArithmExpr:" << endl;
 		ArithmExpr a{ t1, t2 };
 		cout << "infix: \" ";
 		a.print_infix();
-		cout << " \"\n";
+		cout << " \"" << endl;
 		cout << "postfix: \" ";
 		a.print_postfix();
-		cout << " \"\n";*/
-		//cout << "result: "; cout << a.calculate() << "\n";
+		cout << " \"" << endl;
+		cout << "result: "; cout << a.calculate();
+		cout << endl << endl;
 	}
 	return 0;
 }
--- ArithmExpr/ArithmExpr.cpp
+++ ArithmExpr/ArithmExpr.cpp
@@ -44,46 +44,74 @@ void ArithmExpr::print_postfix(ostream& out) const {
 	print(out, postfix);
 }
 
+double get_value(Token t) {
+	if (t.get_type() == TokenType::Number)
+		return t.get_val();
+	else if (t.get_type() == TokenType::Name)
+		return table[t.get_str()];
+	else
+		throw -1;
+}
+
 double ArithmExpr::calculate() {
-	Stack<double> s;
+	Stack<Token> s;
 
 	for (const auto& o : postfix) {
 		switch (o.get_type())
 		{
 		case TokenType::Number:
-			s.push(o.get_val());
+		case TokenType::Name:
+			s.push(o);
 			break;
 		case TokenType::Un_Operator:
-			double x;
-			x = s.pop();
+		{
+			auto x = s.pop();
 			if (o.get_str() == "-")
-				s.push(-x);
-			else
-				s.push(x);
+				s.push(Token{ TokenType::Number, -get_value(x) });
+			else //if (o.get_str() == "+")
+				s.push(Token{ TokenType::Number, get_value(x) });
 			break;
+		}
 		case TokenType::Bn_Operator:
-			double Y;
-			Y = s.pop();
-			double X;
-			X = s.pop();
-			if (o.get_str() == "+") {
-				s.push(X + Y);
-			}
-			else if (o.get_str() == "-") {
-				s.push(X - Y);
-			}
-			else if (o.get_str() == "*") {
-				s.push(X * Y);
-			}
-			else if (o.get_str() == "/") {
-				s.push(X / Y);
-			}
+		{
+			if (s.empty())
+				throw -1;
+			auto Y = s.pop();
+			if (s.empty())
+				throw -1;
+			auto X = s.pop();
+			if (o.get_str() == "+")
+				s.push(Token{ TokenType::Number, get_value(X) + get_value(Y) });
+			else if (o.get_str() == "-")
+				s.push(Token{ TokenType::Number, get_value(X) - get_value(Y) });
+			else if (o.get_str() == "*")
+				s.push(Token{ TokenType::Number, get_value(X) * get_value(Y) });
+			else if (o.get_str() == "/")
+				s.push(Token{ TokenType::Number, get_value(X) / get_value(Y) });
+			break;
+		}
+		case TokenType::Sp_Operator:
+		{
+			if (o.get_str() != "=")
+				throw -1;
+			if (s.empty())
+				throw -1;
+			auto Y = s.pop();
+			if (s.empty())
+				throw -1;
+			auto X = s.pop();
+			if (X.get_type() != TokenType::Name)
+				throw -1;
+			table[X.get_str()] = get_value(Y);
+			s.push(X);
+			break;
+		}
 		default:
 			break;
 		}
 	}
 
-	return s.pop();
+	return get_value(s.pop());
 }
 
 vector<ArithmExpr> get_ArithmExpr_vector(const string& str) {
